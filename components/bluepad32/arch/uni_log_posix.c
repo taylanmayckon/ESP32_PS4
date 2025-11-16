e6b5e97a42a726e52047f20d55d47d7b83af1e83
taylanmayckon <eusoutaylann@gmail.com>
2025-11-16 01:51:52 -0300

    Delete(components): Removendo dependencia desnecessaria no repositorio remoto

--- components/bluepad32/arch/uni_log_posix.c
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright 2022 Ricardo Quesada
-// http://retro.moe/unijoysticle2
-
-#include "uni_log.h"
-
-void uni_logv(const char* fmt, va_list args) {
-    vfprintf(stdout, fmt, args);
-}
--- components/bluepad32/uni_version.c
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright 2021 Ricardo Quesada
-// http://retro.moe/unijoysticle2
-
-#include "uni_version.h"
-
-const char* uni_version = UNI_VERSION_STRING;
